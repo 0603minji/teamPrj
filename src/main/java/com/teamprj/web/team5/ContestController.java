6374910d12803f5b2194c2a770bec73ac7bb9188
jvn1o <junok0987@naver.com>
2024-08-07 18:22:59 +0900

    0807

--- src/main/java/com/teamprj/web/team5/ContestController.java
+++ src/main/java/com/teamprj/web/team5/ContestController.java
@@ -20,4 +20,9 @@ public class ContestController {
     public String contestList(){
         return "HaneulWalk/contest/list";
     }
+
+    @GetMapping("feed")
+    public String contestFeed(){
+        return "HaneulWalk/contest/feed";
+    }
 }
