da00575e096515c69f596cccf04e606de4d0b12f
jvn1o <junok0987@naver.com>
2024-08-08 18:35:06 +0900

    jaebok merge

--- src/main/java/com/teamprj/web/team5/HomeController.java
+++ src/main/java/com/teamprj/web/team5/HomeController.java
@@ -2,27 +2,14 @@ package com.teamprj.web.team5;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-@RequestMapping("/haneulwalk")
+
 public class HomeController {
-    
-    @GetMapping("index")
-    public String homeIndex(){
+    @GetMapping("haneulwalk")
+    public String home(){
         return "HaneulWalk/index";
     }
-    
-    @GetMapping("category")
-    public String category(){
-        return "HaneulWalk/category";
-    }
-    
-    @GetMapping("diary/index")
-    public String diary(){
-        return "HaneulWalk/diary/index";
-    }
-    
     
 }
